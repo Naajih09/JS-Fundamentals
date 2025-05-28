24a043546fce18048e86dc0f4e0507151e4107fb
Naajih Siraj <Naajihibnsiraj@gmail.com>
2025-05-28 01:01:28 +0100

    Updated

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,9 +1,8 @@
 const x = Number(process.argv[2]);
 
-if (!Number.isInteger(x) || x <= 0) {
-  console.log("Missing number of occurrences");
-} else {
+if (Number.isInteger(x) && x > 0) {
   for (let i = 0; i < x; i++) {
     console.log("C is fun");
   }
 }
+
